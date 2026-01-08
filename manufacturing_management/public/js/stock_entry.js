05a889ab10999cd05249fa4b2efba1e64622d5fc
Olatunji Komolafe <iamkomolafe.o.s@gmail.com>
2026-01-08 09:09:37 +0100

    Sets default difference account for specific entry types
    Ensures items have a predefined difference account when entry type
    is Material Transfer or Manufacture, promoting consistency and
    reducing manual assignment errors.

--- manufacturing_management/public/js/stock_entry.js
+++ manufacturing_management/public/js/stock_entry.js
@@ -15,5 +15,12 @@ frappe.ui.form.on("Stock Entry", {
 				},
 			});
 		}
+	},
+	validate(frm) {
+		frm.doc.items.forEach(function (item) {
+			if (["Material Transfer", "Manufacture"].includes(frm.doc.stock_entry_type)) {
+				item.difference_account = "522116 - Stock Adjustment - SSC";
+			}
+		});
 	}
 });
